ea0e5c2cfe526f67d2c539af03486ef2586ef9a7
remy <remy.lille@laposte.net>
2025-05-12 15:06:54 +0200

    Exo51

--- src/main/java/org/example/Exo51.java
+++ src/main/java/org/example/Exo51.java
@@ -3,5 +3,11 @@ package org.example;
 public class Exo51 {
     public static void main(String[] args) {
 
+        String nom, prenom;
+        nom = "doe";
+        prenom = "john";
+
+        System.out.println(" hello c'est " + prenom + ' ' + nom );
+
     }
 }
