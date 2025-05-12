c3c0e566835f46436f9876ea8f7aee6fe71c7b3d
remy <remy.lille@laposte.net>
2025-05-12 15:23:49 +0200

    Exo53

---
+++ src/main/java/org/example/exo53.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class exo53 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a =2;
+        b= 3;
+        c = a*a + b*b;
+        System.out.println( " a est =  " +a );
+        System.out.println(" b est =  " +b);
+
+        System.out.println( " la somme des carrés de a et b  est " + c );
+    }
+}
